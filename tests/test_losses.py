2c06a65e156aa033be7746aff36b0874ae06fa98
Matti <matti.kaupenjohann@fh-dortmund.de>
2022-05-07 18:21:55 +0200

    Update test_losses.py
    Add test setup fixtures for upcoming tests

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -40,3 +40,70 @@ test scenario for overlapping_loss:
 
     expected result = 1
 """
+
+import numpy as np
+import pytest
+
+# Fixture Setup
+
+
+@pytest.fixture()
+def bounding_box() -> np.array:
+    """Pytest fixture of 2x2 BoundingBox."""
+    return np.array([[0, 1], [0, 0]])
+
+
+@pytest.fixture()
+def bounding_box3() -> np.array:
+    """Pytest fixture of 2x2 BoundingBox."""
+    return np.array([[1, 1, 1], [1, 0, 2], [2, 2, 2]])
+
+
+@pytest.fixture()
+def multi_cand() -> np.array:
+    """Pytest fixture of 2x2 BoundingBox."""
+    return np.array([[1, 1, 1], [2, 0, 1], [2, 2, 2]])
+
+
+@pytest.fixture()
+def overlap_cand() -> np.array:
+    """Pytest fixture of 2x2 overlapping candidate."""
+    return np.array([[0, 1], [0, 1]])
+
+
+@pytest.fixture()
+def not_overlap_cand() -> np.array:
+    """Pytest fixture of 2x2 not overlapping candidate."""
+    return np.array([[0, 0], [1, 0]])
+
+
+@pytest.fixture()
+def full_overlap_cand() -> np.array:
+    """Pytest fixture of 2x2 full overlapping candidate."""
+    return np.array([[0, 1], [0, 0]])
+
+
+@pytest.fixture()
+def not_overlap_cands(not_overlap_cand: np.array) -> np.array:
+    """Pytest fixture of 3 2x2 not overlapping candidates."""
+    return np.array((not_overlap_cand, not_overlap_cand, not_overlap_cand))
+
+
+@pytest.fixture()
+def mixed_overlap_cands(
+    not_overlap_cand: np.array, overlap_cand: np.array, full_overlap_cand: np.array
+) -> np.array:
+    """Pytest fixture of 3 2x2 mixed overlapping candidates."""
+    return np.array((not_overlap_cand, overlap_cand, full_overlap_cand))
+
+
+@pytest.fixture()
+def full_overlap_cands(full_overlap_cand: np.array) -> np.array:
+    """Pytest fixture of 3 2x2 not overlapping candidates."""
+    return np.array((full_overlap_cand, full_overlap_cand, full_overlap_cand))
+
+
+@pytest.fixture()
+def diff_label_cands(full_overlap_cand: np.array) -> np.array:
+    """Pytest fixture of 3 2x2 not overlapping candidates."""
+    return np.array((full_overlap_cand * 2, full_overlap_cand * 2, full_overlap_cand * 2))
